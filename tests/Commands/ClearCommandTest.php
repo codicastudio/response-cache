e204b0eb79a912cec11ce917848491295c8fe8ac
codica-studio <CodicaStudioDev@gmail.com>
2020-09-21 00:53:23 -0500

    Initial

---
+++ tests/Commands/ClearCommandTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Spatie\ResponseCache\Test\Commands;
+
+use Event;
+use Illuminate\Cache\Repository;
+use Illuminate\Support\Facades\Artisan;
+use Spatie\ResponseCache\Events\ClearedResponseCache;
+use Spatie\ResponseCache\Events\ClearingResponseCache;
+use Spatie\ResponseCache\ResponseCacheRepository;
+use Spatie\ResponseCache\Test\TestCase;
+
+class ClearCommandTest extends TestCase
+{
+    /** @test */
+    public function it_will_clear_the_cache()
+    {
+        $firstResponse = $this->call('GET', '/random');
+
+        Artisan::call('responsecache:clear');
+
+        $secondResponse = $this->call('GET', '/random');
+
+        $this->assertRegularResponse($firstResponse);
+        $this->assertRegularResponse($secondResponse);
+
+        $this->assertDifferentResponse($firstResponse, $secondResponse);
+    }
+
+    /** @test */
+    public function it_will_fire_events_when_clearing_the_cache()
+    {
+        Event::fake();
+
+        Artisan::call('responsecache:clear');
+
+        Event::assertDispatched(ClearingResponseCache::class);
+        Event::assertDispatched(ClearedResponseCache::class);
+    }
+
+    /** @test */
+    public function it_will_clear_all_when_tags_are_not_defined()
+    {
+        $responseCache = $this->createTaggableResponseCacheStore(null);
+        $appCache = $this->app['cache']->store('array');
+
+        $appCache->forever('appData', 'someValue');
+        $responseCache->clear();
+
+        $this->assertNull($appCache->get('appData'));
+    }
+
+    protected function createTaggableResponseCacheStore($tag): Repository
+    {
+        $this->app['config']->set('responsecache.cache_store', 'array');
+        $this->app['config']->set('responsecache.cache_tag', $tag);
+
+        // Simulating construction of Repository inside of the service provider
+        return $this->app->contextual[ResponseCacheRepository::class][$this->app->getAlias(Repository::class)]();
+    }
+}
---
+++ tests/ResponseSerializerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Spatie\ResponseCache\Test;
+
+use Config;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Spatie\ResponseCache\Exceptions\CouldNotUnserialize;
+use Spatie\ResponseCache\Serializers\Serializer;
+use Spatie\ResponseCache\Test\Serializers\TestSerializer;
+
+class ResponseSerializerTest extends TestCase
+{
+    protected string $textContent;
+
+    protected string $jsonContent;
+
+    protected string $statusCode;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->textContent = '<html>This is a response</html>';
+        $this->jsonContent = json_encode(['text' => 'This is a response']);
+
+        $this->statusCode = 500;
+    }
+
+    /** @test */
+    public function it_can_serialize_and_unserialize_a_response()
+    {
+        // Instantiate a default serializer
+        $responseSerializer = app(Serializer::class);
+
+        $testResponse = Response::create(
+            $this->textContent,
+            $this->statusCode,
+            ['testHeader' => 'testValue']
+        );
+
+        $serializedResponse = $responseSerializer->serialize($testResponse);
+
+        $this->assertTrue(is_string($serializedResponse));
+
+        $unserializedResponse = $responseSerializer->unserialize($serializedResponse);
+
+        $this->assertInstanceOf(Response::class, $unserializedResponse);
+
+        $this->assertEquals($this->textContent, $unserializedResponse->getContent());
+
+        $this->assertEquals($this->statusCode, $unserializedResponse->getStatusCode());
+
+        $this->assertEquals('testValue', $unserializedResponse->headers->get('testHeader'));
+    }
+
+    /** @test */
+    public function it_can_customized_serialize_and_unserialize_a_response()
+    {
+        // Set config dynamically for test
+        Config::set('responsecache.serializer', TestSerializer::class);
+
+        // Instantiate a custom serializer according to config
+        $responseSerializer = app(Serializer::class);
+
+        $testResponse = JsonResponse::create(
+            $this->jsonContent,
+            $this->statusCode,
+            ['testHeader' => 'testValue']
+        );
+
+        $serializedResponse = $responseSerializer->serialize($testResponse);
+
+        $this->assertTrue(is_string($serializedResponse));
+
+        $unserializedResponse = $responseSerializer->unserialize($serializedResponse);
+
+        $this->assertInstanceOf(JsonResponse::class, $unserializedResponse);
+
+        $this->assertEquals($this->jsonContent, $unserializedResponse->getData());
+
+        $this->assertEquals($this->statusCode, $unserializedResponse->getStatusCode());
+
+        $this->assertEquals('testValue', $unserializedResponse->headers->get('testHeader'));
+    }
+
+    /** @test */
+    public function it_throws_an_exception_when_something_else_than_a_response_is_unserialized()
+    {
+        $this->expectException(CouldNotUnserialize::class);
+
+        app(Serializer::class)->unserialize('b:0;');
+    }
+}
